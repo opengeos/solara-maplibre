8030b057564333ee55b691f5a9dd6572e002b8a4
Qiusheng Wu <giswqs@gmail.com>
2025-05-25 13:33:19 -0400

    Update home page

--- pages/00_home.py
+++ pages/00_home.py
@@ -5,9 +5,11 @@ import solara
 def Page():
     with solara.Column(align="center"):
         markdown = """
-        ## A Solara Template for MapLibre
+        ## 3D Mapping with Leafmap and MapLibre
 
+        This is a Solara template for a 3D mapping application using Leafmap and MapLibre. Click on the menu above to see the different examples.
 
+        ![image](https://github.com/user-attachments/assets/efc9e43b-99c0-40b4-af08-4971e8b96919)
         """
 
         solara.Markdown(markdown)
