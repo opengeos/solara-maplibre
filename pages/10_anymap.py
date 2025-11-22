f5dae26655740d5d059d63d8ce229d0a7cc4994d
Qiusheng Wu <giswqs@gmail.com>
2025-11-22 13:29:43 -0500

    Update anymap version

--- pages/10_anymap.py
+++ pages/10_anymap.py
@@ -4,9 +4,14 @@ from anymap import MapLibreMap
 
 class Map(MapLibreMap):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(
+            center=[-117.592133766, 47.653004], zoom=15.3, height="750px", *kwargs
+        )
         super().add_basemap("Esri.WorldImagery")
-        super().add_layer_control(collapsed=False)
+        super().add_layer_control(collapsed=True)
+        super().add_geoman_control(
+            collapsed=True, show_info_box=True, info_box_mode="click"
+        )
 
 
 @solara.component
